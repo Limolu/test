30d4178041f65ba01dc705f92b67513301d8b3fb
Limolu <87972280+Limolu@users.noreply.github.com>
2021-09-11 23:04:44 +0800

    Add files via upload

--- one.js
+++ one.js
@@ -1,5 +1,6 @@
-var my_name = prompt("你的名字")
-var my_age = "20"
+var my_name = prompt("你叫甚麼名字")
+var my_age = prompt("你今年幾歲？")
 var is_male = true
 
-document.write("我的名字叫做" + my_name)
+document.write("你好！" + my_name + "，非常高興見到你！")
+document.write("聽說你今年" + my_age + "歲是吧？這麼說起來，白白好像是史萊姆婆")
